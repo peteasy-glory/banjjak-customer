8afa4c50a9838b56f75f23d9011e41ac8b81b895
eaden <eaden@peteasy.kr>
2022-05-30 17:57:07 +0900

    월내 일별 산출 & 그래프 보기

--- walking/daily/index.php
+++ walking/daily/index.php
@@ -9,7 +9,7 @@ $user_name = (isset($_SESSION['gobeauty_user_nickname'])) ? $_SESSION['gobeauty_
 
 $api = new TRestAPI("http://stg-walkapi.banjjakpet.com:8080", "token 58de28d6170dcf11edf7c009bff81e37536a2fa4");
 //$api = new TRestAPI("http://192.168.20.128:8080", "token 58de28d6170dcf11edf7c009bff81e37536a2fa4");
-$mypets = $api->get("/walklog/pet/".$user_id);
+$mypets = $api->get("/walklog/pets/".$user_id);
 $_SESSION['backurl_shop'] = $_SERVER[ "REQUEST_URI" ];
 
 $pet_id = $_GET['pet'];
@@ -18,6 +18,8 @@ $log_year = $_GET['year'];
 if($log_year == null){
     $log_year = date("Y");
 }
+
+
 ?>
 
 <header id="header">
@@ -60,7 +62,7 @@ if($log_year == null){
                                 $pet_id = $val["pet_seq"];
                                 $pet_name = $val["name"];
                                 $pet_thumb = $photo;
-                                $mypet_log = $api->get("/walklog/pet/log/".$val['pet_seq']);
+                                $mypet_log = $api->get("/walklog/pet/".$val['pet_seq']);
                                 echo '<div class="list-cell" ><a href="#" id="pet_'.$val["pet_seq"].'" class="btn-user-pet-item actived"><div class="icons"><img src="'.$photo.'" alt=""/></div><div class="txt">'.$name.'</div></a></div>';
                             }else{
                                 echo '<div class="list-cell" ><a href="#" id="pet_'.$val["pet_seq"].'" class="btn-user-pet-item"><div class="icons"><img src="'.$photo.'" alt=""/></div><div class="txt">'.$name.'</div></a></div>';
@@ -103,15 +105,15 @@ if($log_year == null){
                                     <div class="recode-card-data-title">전체 산책</div>
                                     <div class="record-display">
                                         <div class="state-item">
-                                            <div class="state-item-value" id=""><?=$mypet_log['body'][0]['count']?></div>
+                                            <div class="state-item-value" id=""><?=number_format(intval($mypet_log['body'][0]['count']))?></div>
                                             <div class="state-item-label">회</div>
                                         </div>
                                         <div class="state-item">
-                                            <div class="state-item-value"><?=$mypet_log['body'][0]['dist']?></div>
+                                            <div class="state-item-value"><?=number_format(intval($mypet_log['body'][0]['dist'])/1000,2)?></div>
                                             <div class="state-item-label">Km</div>
                                         </div>
                                         <div class="state-item">
-                                            <div class="state-item-value"><?=$mypet_log['body'][0]['time']?></div>
+                                            <div class="state-item-value"><?=number_format(intval($mypet_log['body'][0]['time'])/60, 2)?></div>
                                             <div class="state-item-label">분</div>
                                         </div>
                                     </div>
@@ -120,15 +122,15 @@ if($log_year == null){
                                     <div class="recode-card-data-title">평균/1회 산책</div>
                                     <div class="record-display">
                                         <div class="state-item">
-                                            <div class="state-item-value"><?=intval($mypet_log['body'][0]['dist']/$mypet_log['body'][0]['count'])?></div>
+                                            <div class="state-item-value"><?=number_format(intval($mypet_log['body'][0]['dist']/$mypet_log['body'][0]['count'])/1000, 2)?></div>
                                             <div class="state-item-label">Km</div>
                                         </div>
                                         <div class="state-item">
-                                            <div class="state-item-value"><?=intval($mypet_log['body'][0]['time']/$mypet_log['body'][0]['count'])?></div>
+                                            <div class="state-item-value"><?=number_format(intval($mypet_log['body'][0]['time']/$mypet_log['body'][0]['count'])/60,2)?></div>
                                             <div class="state-item-label">분</div>
                                         </div>
                                         <div class="state-item">
-                                            <div class="state-item-value"><?=intval($mypet_log['body'][0]['poo']/$mypet_log['body'][0]['count'])?></div>
+                                            <div class="state-item-value"><?=number_format(intval($mypet_log['body'][0]['poo']/$mypet_log['body'][0]['count']))?></div>
                                             <div class="state-item-label"><img src="/static/pub/images/icon/icon_defecate.png" alt="" width="24"/></div>
                                         </div>
                                     </div>
@@ -156,7 +158,7 @@ if($log_year == null){
                         </select>
                     </div>
                     <?php
-                        $month_log = $api->get("/walklog/pet/".$user_id."/".$pet_id."/".$log_year);
+                        $month_log = $api->get("/walklog/year/".$user_id."/".$pet_id."/".$log_year);
                         if($month_log['body'] != null){
                             echo '<a href="#" class="btn btn-middle-size btn-outline-gray btn-round btn-graph-view"><span class="icon icon-graph-view"></span>그래프로 보기</a>';
                             echo '<div class="single-btns-list top-none-line">';
@@ -183,7 +185,7 @@ if($log_year == null){
                 <div class="basic-data-group middle">
                     <div class="recode-none">
                         <div class="con-title-group">
-                            <h4 class="con-title">산책 기록이 없습니다.</h4>
+                            <h4 class="con-title">아이에게 산책을 선물하세요.</h4>
                         </div>
                     </div>
                 </div>
@@ -211,6 +213,7 @@ function jsAlert($year){
         if(mobile === "in_app_and" || mobile === "in_app_ios"){
             onBackWalkTop(mobile);
         }else {
+            alert(location.href)
             location.href = "/";
         }
     });
@@ -241,24 +244,53 @@ function jsAlert($year){
         pop.open('firstRequestMsg1');
     });
 
-    $(".btn-single-item").click(function(){
-        var form = document.createElement('form');
-        var objs = document.createElement('input');
-        objs.setAttribute('type', 'hidden');
-        objs.setAttribute('name', 'month_log');
-        objs.setAttribute('value', $(this).attr("id"));
-        form.appendChild(objs);
-        form.setAttribute('method', 'post');
-        form.setAttribute('action', "/walking/daily/history");
-        document.body.appendChild(form);
-        form.submit();
-    });
-
     $(document).on("change", ".arrow", function(){
         const year = $(this).children("option:selected").text().substr(0, 4);
         location.href = "?pet="+<?=$pet_id?>+"&year="+year;
     });
 
+    $(".btn-graph-view").click(function(){
+        const year = $(".arrow").children("option:selected").text().substr(0, 4);
+        var form = document.createElement('form');
+        var objs = document.createElement('input');
+        objs.setAttribute('type', 'hidden');
+        objs.setAttribute('name', 'graph_year');
+        objs.setAttribute('value', year);
+        form.appendChild(objs);
+
+        var objs1 = document.createElement('input');
+        objs1.setAttribute('type', 'hidden');
+        objs1.setAttribute('name', 'pet_id');
+        objs1.setAttribute('value', <?=$pet_id?>);
+        form.appendChild(objs1);
+
+        form.setAttribute('method', 'post');
+        form.setAttribute('action', "/walking/daily/graph.php");
+        document.body.appendChild(form);
+        form.submit();
+    });
+
+
+
+    $(".btn-single-item").click(function(){
+        var form = document.createElement('form');
+        var objs = document.createElement('input');
+        objs.setAttribute('type', 'hidden');
+        objs.setAttribute('name', 'year');
+        objs.setAttribute('value', $(this).attr("id"));
+        form.appendChild(objs);
+
+        var objs1 = document.createElement('input');
+        objs1.setAttribute('type', 'hidden');
+        objs1.setAttribute('name', 'pet_id');
+        objs1.setAttribute('value', <?=$pet_id?>);
+        form.appendChild(objs1);
+
+        form.setAttribute('method', 'post');
+        form.setAttribute('action', "/walking/daily/graph.php");
+        document.body.appendChild(form);
+        form.submit();
+    });
 
     // 기기 체크
     function checkMobile2(){
