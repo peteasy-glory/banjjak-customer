943ce38e799ffdd7f2617cf83fa93aaa0888926f
glory <glory@peteasy.kr>
2022-06-03 17:53:48 +0900

    첫이용상담사진v1

--- common/TAwsS3.php
+++ common/TAwsS3.php
@@ -1,4 +1,9 @@
 <?php
+<<<<<<< Updated upstream
+=======
+//require '/Users/gyeblee/Dev_Project/A01.Project/07.Php/aws/aws-autoloader.php';
+//require '/Users/banjjak_dev/Desktop/Dev_Project/2.Dev/3.Web/www/html/subdomain/banjjak_sol/aws/aws-autoloader.php';
+>>>>>>> Stashed changes
 //require '/var/www/html/subdomain/banjjak_sol/vendor/autoload.php';
 
 use Aws\S3\S3Client;
