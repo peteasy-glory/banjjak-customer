7ef59b83b00537f699b782c3d59b18b145c08b4d
DESKTOP-96CBNAF\USER <lisa@peteasy.kr>
2022-06-07 15:46:30 +0900

    추천아이콘 추가

--- common/TAwsS3.php
+++ common/TAwsS3.php
@@ -1,5 +1,5 @@
 <?php
-require '/var/www/html/subdomain/banjjak_sol/vendor/autoload.php';
+//require '/var/www/html/subdomain/banjjak_sol/vendor/autoload.php';
 //require '/Users/gyeblee/Dev_Project/A01.Project/07.Php/aws/aws-autoloader.php';
 //require '/Users/banjjak_dev/Desktop/Dev_Project/2.Dev/3.Web/www/html/subdomain/banjjak_sol/aws/aws-autoloader.php';
 //require '/var/www/html/subdomain/banjjak_sol/vendor/autoload.php';
