6334d18cb8417a57d2fa6ac6c0b3c360b02f52ea
rolldeep2 <lyw960820@naver.com>
2022-07-25 17:35:38 +0900

    안드로이드 딥링크 수정v2

--- include/source/deeplink.js
+++ include/source/deeplink.js
@@ -20,8 +20,9 @@ let visitedAt = (new Date()).getTime();
 
 
 
+$(document).ready(function(){
 
-//window 거나 and app 이거나 ios app 인경우 다운로드 popup 비활성화
+    //window 거나 and app 이거나 ios app 인경우 다운로드 popup 비활성화
     if(!user_agent.match(/app_gobeauty_and/i) && !user_agent.match(/app_gobeauty_ios/i) && !user_agent.match(/windows/i)){
 
         if(getCookie_popup('appDownloadpopup') !== 'Y'){
@@ -41,36 +42,36 @@ let visitedAt = (new Date()).getTime();
         document.getElementById('appDownloadTop').classList.remove('actived');
     }
 
-$('.layer-pop-jack').click(function (event) {
-    event.stopPropagation();
-
-})
-
-
-if (user_agent.match(/iphone|ipad|ipod/i)) {
-
-
-
-
-    $('.app-download-link').on('click',function (){
-
-        location.href = `banjjakpet:/${needed}`;
-
-        setTimeout(()=>{
-
-            location.href = `itms-apps://itunes.apple.com/kr/app/apple-store/id1436568194`;
-        },1500)
-
-    })
-}else{
-
-    $('.app-download-link').on('click',function(){
-
-        location.href = `intent:/${needed}#Intent;scheme=banjjakpet;action=android.intent.action.VIEW;package=m.kr.gobeauty;end`;
+    $('.layer-pop-jack').click(function (event) {
+        event.stopPropagation();
 
     })
 
-}
+
+    if (user_agent.match(/iphone|ipad|ipod/i)) {
+
+
+
+
+        $('.app-download-link').on('click',function (){
+
+            location.href = `banjjakpet:/${needed}`;
+
+            setTimeout(()=>{
+
+                location.href = `itms-apps://itunes.apple.com/kr/app/apple-store/id1436568194`;
+            },1500)
+
+        })
+    }else{
+
+        $('.app-download-link').on('click',function(){
+
+            location.href = `intent:/${needed}#Intent;scheme=banjjakpet;action=android.intent.action.VIEW;package=m.kr.gobeauty;end`;
+
+        })
+
+    }
 
 
 
@@ -78,75 +79,77 @@ if (user_agent.match(/iphone|ipad|ipod/i)) {
 
     if(document.referrer === ''){
 
-    if(getCookie_popup('anymore') !=='Y') {
+        if(getCookie_popup('anymore') !=='Y') {
 
-        if (user_agent.match(/iphone|ipad|ipod/i)) { //ios 일때
+            if (user_agent.match(/iphone|ipad|ipod/i)) { //ios 일때
 
 
-            if (user_agent.match(/APP_GOBEAUTY_iOS/i)) { //ios app 일때
-                webkit.messageHandlers.SET_MoveMenu.postMessage(idx, email);
+                if (user_agent.match(/APP_GOBEAUTY_iOS/i)) { //ios app 일때
+                    webkit.messageHandlers.SET_MoveMenu.postMessage(idx, email);
 
-            } else { //ios web 일때
+                } else { //ios web 일때
 
-                setTimeout( // 앱이 있으면 앱으로
+                    setTimeout( // 앱이 있으면 앱으로
 
-                    function () {
-                        if ((new Date()).getTime() - visitedAt < 2000) {
-                            location.href = `banjjakpet:/${needed}`;
+                        function () {
+                            if ((new Date()).getTime() - visitedAt < 2000) {
+                                location.href = `banjjakpet:/${needed}`;
+                            }
                         }
-                    }
-                    , 300);
+                        , 300);
 
 
 
 
 
-            }
+                }
 
-        } else if (!user_agent.match(/kakao/i) && user_agent.match(/android/i)) { //안드로이드 일때
+            } else if (!user_agent.match(/kakao/i) && user_agent.match(/android/i)) { //안드로이드 일때
 
-            if (user_agent.match(/APP_GOBEAUTY_AND/i)) { // 안드로이드 app 일때
-                Banjjak_Android.SET_MoveMenu(idx, email);
-            } else { // 안드로이드 web 일때
+                if (user_agent.match(/APP_GOBEAUTY_AND/i)) { // 안드로이드 app 일때
+                    Banjjak_Android.SET_MoveMenu(idx, email);
+                } else { // 안드로이드 web 일때
 
+
+                    setTimeout(function () {
+
+                        if ((new Date()).getTime() - visitedAt < 2000) {
+
+                            location.href = `intent:/${needed}#Intent;scheme=banjjakpet;action=android.intent.action.VIEW;package=m.kr.gobeauty;end`;
+                        }
+
+                    }, 300);
+
+
+
+                }
+
+
+            } else if (user_agent.match(/kakao/i) && user_agent.match(/android/i)) {
+
+                // location.href = 'kakaotalk://inappbrowser/close';
 
                 setTimeout(function () {
 
-                    if ((new Date()).getTime() - visitedAt < 2000) {
+                    if (new Date().getTime() - visitedAt < 2000) {
 
-                        location.href = `banjjakpet:/${needed}`;
+                        location.href = `intent:/${needed}#Intent;scheme=banjjakpet;action=android.intent.action.VIEW;package=m.kr.gobeauty;end`;
                     }
 
                 }, 300);
 
+                setTimeout(function () {
+
+
+                    location.href = `intent://${other_browser[1]}#Intent;scheme=http;package=com.android.chrome;end`;
+
+
+                }, 500);
 
 
             }
 
-
-        } else if (user_agent.match(/kakao/i) && user_agent.match(/android/i)) {
-
-            // location.href = 'kakaotalk://inappbrowser/close';
-
-            setTimeout(function () {
-
-                if (new Date().getTime() - visitedAt < 2000) {
-
-                    location.href = `intent:/${needed}#Intent;scheme=banjjakpet;action=android.intent.action.VIEW;package=m.kr.gobeauty;end`;
-                }
-
-            }, 300);
-
-            setTimeout(function () {
-
-
-                location.href = `intent://${other_browser[1]}#Intent;scheme=http;package=com.android.chrome;end`;
-
-
-            }, 500);
-
-
         }
-
     }
-}
+
+})
