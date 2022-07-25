ca3c601fefe0576cbc6550b44c17fdcfa1fbc1e5
rolldeep2 <lyw960820@naver.com>
2022-07-25 17:56:19 +0900

    안드로이드 딥링크 수정v4

--- include/source/deeplink.js
+++ include/source/deeplink.js
@@ -55,7 +55,7 @@ $(document).ready(function(){
 
         $('.app-download-link').on('click',function (){
 
-            location.href = `intent:/${needed}#Intent;scheme=banjjakpet;action=android.intent.action.VIEW;package=m.kr.gobeauty;end`;
+            location.href = `banjjakpet:/${needed}`;
 
             setTimeout(()=>{
 
