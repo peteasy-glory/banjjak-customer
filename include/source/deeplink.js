7eec684c2bdd41529b59b9ac2fdb27657f535d4b
rolldeep2 <lyw960820@naver.com>
2022-07-26 11:02:54 +0900

    캠페인 딥링크 추가 v2

--- include/source/deeplink.js
+++ include/source/deeplink.js
@@ -1,3 +1,28 @@
+function getCookie_popup(name) {
+    var obj = name + "=";
+    var x = 0;
+    while (x <= document.cookie.length) {
+        var y = (x + obj.length);
+        if (document.cookie.substring(x, y) == obj) {
+            if ((endOfCookie = document.cookie.indexOf(";", y)) == -1)
+                endOfCookie = document.cookie.length;
+            return unescape(document.cookie.substring(y, endOfCookie));
+        }
+        x = document.cookie.indexOf(" ", x) + 1;
+        if (x == 0)
+            break;
+    }
+    return "";
+}
+
+function setCookie_popup(name, value, expiredays) {
+    var todayDate = new Date();
+    todayDate.setDate(todayDate.getDate() + expiredays);
+    document.cookie = name + '=' + escape(value) + '; path=/; expires=' + todayDate.toGMTString() + ';';
+
+}
+
+
 let user_agent = navigator.userAgent.toLowerCase();
 
 
